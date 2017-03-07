9069e34ad19574f947b6b1a1710370d269162b71
Thomas Komarnicki <tkomarnicki@hanekedesign.com>
2017-03-07 15:14:28 -0500

    add temporary mock android project

---
+++ src/test/groovy/com/hanekedesign/build/HanekeDemoUploadTest.groovy
@@ -0,0 +1,41 @@
+package com.hanekedesign.build
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+
+
+class HanekeDemoUploadTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testUpload(){
+        def project = MockProjects.CreateMockProject()
+
+        project.evaluate()
+
+
+        DemoFtpUploader demoFtpUploader = new DemoFtpUploader(Sensitive.FTP_ADDRESS, Sensitive.FTP_USER, Sensitive.FTP_PASSWORD)
+        demoFtpUploader.uploadFile("demo.hanekedesign.com/Android/Test/Test.apk", new File("test.apk"))
+    }
+
+    @Test
+    public void testPluginCreation(){
+        def project = MockProjects.CreateMockProject()
+        project.evaluate()
+
+        def debugTask = project.tasks.getByName("hanekeFtpUploadDebug")
+        def releaseTask = project.tasks.getByName("hanekeFtpUploadRelease")
+
+        Assert.assertTrue(debugTask != null)
+        Assert.assertTrue(releaseTask != null)
+
+    }
+
+
+}
